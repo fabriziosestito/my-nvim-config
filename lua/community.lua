f24fb6d4e06792ab8104221777a7b0eb1c156c3c
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-03-20 08:41:27 +0100

    first commit
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

---
+++ lua/community.lua
@@ -0,0 +1,21 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  -- { import = "astrocommunity.recipes.ai" },
+  { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.harper" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+  { import = "astrocommunity.pack.markdown" },
+}
